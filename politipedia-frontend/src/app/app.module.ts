1f9240f3370a1a690e237b9add11d5874541c4c1
Avishka Suduwa Dewage <avishka@utexas.edu>
2019-02-27 15:51:26 -0600

    fixed compilation error (unkown tag)

--- politipedia-frontend/src/app/app.module.ts
+++ politipedia-frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { TrendingComponent } from './trending/trending.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
-
 import { AboutComponent } from './about/about.component';
 import { TabsComponent } from './tabs/tabs.component';
 
