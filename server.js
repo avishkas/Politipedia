9802c137092c0469126b2d9e68d191a77db853e7
avishkas <avishkas5@gmail.com>
2019-03-24 15:51:48 -0500

    edited candidate endpoint to retrieve sql data

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('./Politipedia/politipedia-frontend/dist/politipedia-frontend'))
+const mysql = require('mysql');
+// connection configurations
+const mc = mysql.createConnection({
+    host     : 'mydbinstance2.c4rwwzce94u4.us-east-2.rds.amazonaws.com',
+    user     : 'meberlein',
+    password : 'EE461LEE461L',
+    database : 'Politipedia'
+});
 
-app.get('/callme', (req, res) => res.send('Hello World!'))
+app.use(express.static('./Politipedia/politipedia-frontend/dist/politipedia-frontend'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/callme', (req, res) => res.send('Hello World!'));
+
+app.get('/search/candidate', (req, res) => {
+   var candidateName = req.query['candidate-name'];
+
+   mc.query("Select * from Candidate", function (err, rows, fields) {
+
+
+       if(err)
+           res.status(500).send({error: "error querying database"});
+
+       res.json(rows);
+   });
+   //query database;
+
+});
+
+app.get('/search/elections', (req, res) => {
+    var electionYear = req.query('election-year');
+
+});
+
+app.get('/search/bills', (req, res) => {
+});
+
+app.get('/search/donor', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 
