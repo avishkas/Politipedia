3e38ebe12a335d68ee0e80691b07e2980916890c
justin <justin.jy.chen@utexas.edu>
2019-03-25 21:55:47 -0500

    dynamically retrieves git issues

--- politipedia-frontend/src/app/bio/bio.component.ts
+++ politipedia-frontend/src/app/bio/bio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient , HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
+import {createUrlResolverWithoutPackagePrefix} from "@angular/compiler";
 
 
 @Component({
@@ -10,9 +11,8 @@ import {Observable} from "rxjs";
 })
 export class BioComponent implements OnInit {
 
-  gitObservables : Observable<any>;
-  totalCommitObservables : Observable<any>;
-  output : object;
+  gitCommits : Observable<any>;
+  gitIssues : Observable<any>;
 
   justinCommits = '0';
   avishkaCommits = '0';
@@ -22,11 +22,24 @@ export class BioComponent implements OnInit {
   andyCommits = '0';
   totalCommits = 0;
 
+  justinIssues = 0;
+  avishkaIssues = 0;
+  ashwinIssues = 0;
+  isabelIssues = 0;
+  christineIssues = 0;
+  andyIssues = 0;
+  totalIssues = 0;
+
   constructor(private httpClient:HttpClient) { }
 
   ngOnInit() {
-    this.gitObservables = this.httpClient.get<any>("https://api.github.com/repos/avishkas/politipedia/contributors");
-    this.gitObservables.subscribe((data) => {
+    this.getGitCommits();
+    this.getGitIssues();
+  }
+
+  public getGitCommits(){
+    this.gitCommits = this.httpClient.get<any>("https://api.github.com/repos/avishkas/politipedia/contributors");
+    this.gitCommits.subscribe((data) => {
       for(let i=0;i<data.length;i++){
         //Switch cases for each user
         switch(data[i]["login"]) {
@@ -58,15 +71,48 @@ export class BioComponent implements OnInit {
       }
       this.totalCommits = eval(this.justinCommits) + eval(this.avishkaCommits) + eval(this.ashwinCommits) + eval(this.isabelCommits) + eval(this.christineCommits);
     });
+  }
 
-
-    //   this.totalCommitObservables = this.httpClient.get<any>("https://api.github.com/repos/avishkas/politipedia/stats/commit_activity");
-    //
-    //   this.totalCommitObservables.subscribe((data) => {
-    //     for(let i=0; data.length; i++){
-    //       this.totalCommits += eval(data[i]["total"]);
-    //     }
-    //   })
-    // }
+  public getGitIssues(){
+    this.gitIssues = this.httpClient.get<any>("https://api.github.com/repos/avishkas/politipedia/issues");
+    this.gitIssues.subscribe((data) => {
+      console.log(data);
+      for(let i=0;i<data.length;i++){
+        let currentUser = data[i]["user"]["login"];
+        console.log(currentUser);
+        switch(currentUser) {
+          case "avishkas": {
+            this.avishkaIssues++;
+            this.totalIssues++;
+            break;
+          }
+          case "JustinJChen": {
+            this.justinIssues++;
+            this.totalIssues++;
+            break;
+          }
+          case "Shwinn": {
+            this.ashwinIssues++;
+            this.totalIssues++;
+            break;
+          }
+          case "Yuan-Chang-UT": {
+            this.andyIssues++;
+            this.totalIssues++;
+            break;
+          }
+          case "imabelli": {
+            this.isabelIssues++;
+            this.totalIssues++;
+            break;
+          }
+          case "daochristine039": {
+            this.christineIssues++;
+            this.totalIssues++;
+            break;
+          }
+        }
+      }
+    });
   }
 }
