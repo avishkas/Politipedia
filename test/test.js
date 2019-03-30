3daf4632df94b51f59452c52fdfe2e6286cc182c
[daochristine039] <[daochristine039@utexas.edu]>
2019-03-30 17:37:47 -0500

    Mocha test.js functional

--- test/test.js
+++ test/test.js
@@ -11,29 +11,62 @@ describe("SAMPLE unit test",function(){
 
     // #1 should return home page
 
-    it("should return home page",function(done){
+    it("should return home page",function(){
 
         // calling home page api
         server
             .get("/")
-            .expect("Content-type",/json/)
             .expect(200) // THis is HTTP response
             .end(function(err,res){
                 // HTTP status should be 200
                 res.status.should.equal(200);
                 // Error key should be false.
                 res.body.error.should.equal(false);
-                done();
             });
     });
 
-    it("should return 404",function(done) {
+    it("should return 404",function() {
         server
             .get("/random")
             .expect(404)
             .end(function (err, res) {
                 res.status.should.equal(404);
-                done();
+            });
+    });
+
+    it("should return to candidate result page",function() {
+        server
+            .get("/candidate-result")
+            .expect(200)
+            .end(function(err,res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+            });
+    });
+
+    it("should return to election result page",function() {
+        server
+            .get("/election-result")
+            .expect(200)
+            .end(function(err,res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+            });
+    });
+
+    it("should return to sponsor result page",function() {
+        server
+            .get("/donor-result")
+            .expect(200)
+            .end(function(err,res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
             });
     });
 
