493528c22746671939e95e9fca5d7f78c521af62
justin <justin.jy.chen@utexas.edu>
2019-04-22 12:10:21 -0500

    linked API to donor page

--- politipedia-frontend/src/app/donor/donor.component.ts
+++ politipedia-frontend/src/app/donor/donor.component.ts
@@ -10,12 +10,14 @@ export class DonorComponent implements OnInit {
 
   donorName: string;
   imageURL: string;
+  contributionAmount: string;
 
   constructor(private APIService: ApiService) {}
 
   ngOnInit() {
     this.getDonorName();
     this.getImage();
+    this.getDonorInfo();
   }
   getImage() {
     this.APIService.getImage(this.donorName).subscribe(
@@ -27,6 +29,14 @@ export class DonorComponent implements OnInit {
   }
   getDonorName() {
     this.donorName = sessionStorage.getItem('donorName');
+  }
+  getDonorInfo() {
+    this.APIService.getDonors(this.donorName).subscribe(
+      (data) => {
+        console.log(data);
+        this.contributionAmount = data[0].contribution;
+      }
+    );
   }
 
 }
